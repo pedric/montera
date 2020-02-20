f9930fb5b426548c88c2f6d00f99dce4af34eb3c
Fredrik Larsson <epost.larsson@gmail.com>
2020-02-20 20:18:20 +0530

    added slick slider and skipping some zurb foundation stuff

--- gulpfile.js
+++ gulpfile.js
@@ -4,22 +4,22 @@ var concat = require('gulp-concat');
 var imagemin = require('gulp-imagemin');
 var cleanCSS = require('gulp-clean-css');
 
-gulp.watch("src/css/*.scss", ['sass', 'critical_sass']);
-gulp.watch("src/css/*/*.scss", ['sass', 'critical_sass']);
+gulp.watch("src/css/*.scss", ['sass']);
+gulp.watch("src/css/*/*.scss", ['sass']);
 gulp.watch("src/js/*.js", ['js']);
 gulp.watch("src/img/*", ['img']);
 
 // FOUNDATION BASE CSS
-gulp.task('critical_sass', function() {
-  return gulp.src("src/css/app.scss")
-    .pipe(sass())
-    .pipe(cleanCSS())
-    .pipe(gulp.dest("assets/css"))
-});
+// gulp.task('critical_sass', function() {
+//   return gulp.src("src/css/app.scss")
+//     .pipe(sass())
+//     .pipe(cleanCSS())
+//     .pipe(gulp.dest("assets/css"))
+// });
 
 // CRITICAL CSS
 gulp.task('sass', function() {
-  return gulp.src("src/css/critical.scss")
+  return gulp.src("src/css/*.scss")
     .pipe(sass())
     .pipe(cleanCSS())
     .pipe(gulp.dest("assets/css"))
@@ -28,7 +28,12 @@ gulp.task('sass', function() {
 // JS
 gulp.task('js', function() {
   return gulp.src("src/js/*.js")
-    .pipe(concat('app.js'))
+    .pipe(gulp.dest("assets/js"))
+});
+
+// JS.MAP
+gulp.task('js-map', function() {
+  return gulp.src("src/js/*.map")
     .pipe(gulp.dest("assets/js"))
 });
 
@@ -39,4 +44,4 @@ gulp.task('img', function() {
           .pipe(gulp.dest('assets/img'))
 });
 
-gulp.task('default', ['sass', 'critical_sass', 'js', 'img']);
+gulp.task('default', ['sass', 'js', 'img', 'js-map']);
